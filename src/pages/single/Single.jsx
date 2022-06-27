3291419a7ba628ef6595de8ca636927917f33487
sauloeniefi <sauloeniefi@gmail.com>
2022-06-27 20:05:23 -0300

    Criação tela PUT players

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,30 +1,281 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState} from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./single.scss";
 
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
 const Single = (props) => {
-  
   const location = useLocation();
 
-  useEffect(()=>{
-    console.log("Location from all players", location)
-  },[location])
+  // useEffect(() => {
+  //   console.log("Location from all players", location);
+  // }, [location]);
 
-  return <div className="single">
-    <Sidebar />
-    <div className="singleContainer">
-      <Navbar />
-      <div className="top">
-        <h1 className="title">{location.state.playerData.NAME}</h1>
-        <div className="item"></div>
+  const [playerDataById, setPlayerDataById] = useState([]);
+
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`http://192.168.163.128:3003/api/players/${location.state.playerData.ID}`);
+      const content = await response.json();
+      setPlayerDataById(content);
+      console.log(playerDataById)
+    })();
+  },[location]);
+
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <h1 className="title">{playerDataById.Name}</h1>
+          <div className="item"></div>
+        </div>
+
+        <div className="bottom">
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Name"
+                defaultValue={playerDataById.Name}
+              />
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="Password"
+                defaultValue="********"
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="Access"
+                defaultValue={playerDataById.Access}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Title"
+                defaultValue={playerDataById.Title}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Level"
+                defaultValue={playerDataById.LEVEL}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Exp"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Gender"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Gold"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Coins"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Crystals"
+                defaultValue={location.state.playerData.NAME}
+              />
+            </div>
+
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="HairID"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="ColorHair"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="ColorSkin"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="ColorEye"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="ColorBase"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="ColorTrim"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="ColorAccessory"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="NameColor"
+                defaultValue={location.state.playerData.NAME}
+              />
+            </div>
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="CpBoostExpire"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="RepBoostExpire"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="GoldBoostExpire"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="ExpBoostExpire"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="UpgradeExpire"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="UpgradeDays"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Upgraded"
+                defaultValue={location.state.playerData.NAME}
+              />
+            </div>
+
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="KillCount"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="DeathCount"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="Score"
+                defaultValue={location.state.playerData.NAME}
+              />
+
+              <TextField
+                required
+                id="outlined-required"
+                label="ArenaPoints"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="PvPRank"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="PopularRank"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="PRank"
+                defaultValue={location.state.playerData.NAME}
+              />
+              <TextField
+                required
+                id="outlined-required"
+                label="ARank"
+                defaultValue={location.state.playerData.NAME}
+              />
+            </div>
+          </Box>
+        </div>
       </div>
-      <div className="bottom"></div>
-      single
     </div>
-  </div>;
+  );
 };
 
 export default Single;
